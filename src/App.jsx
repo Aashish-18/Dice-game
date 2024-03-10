490680319db8625a2310ef092f003127ff7951de
Aashish-18 <aashishjha095@gmail.cpm>
2024-03-10 19:24:28 +0530

    completed

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import Start from "./components/Start";
+import GamePlay from "./components/Gameplay";
+function App() {
+  const [isGameStarted, setIsGameStarted] =useState(false);
+  const toggleGamePlay =() => {
+    setIsGameStarted((prev)=>!prev);
+  };
+
+  return (
+    <>
+    {isGameStarted ? <GamePlay /> : <Start 
+    toggle={toggleGamePlay}/>}
+    </>
+  );
+}
+
+export default App
